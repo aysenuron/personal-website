5ff5c93c7b588080cb2fdc4182ebc3d83525c266
aysenuron <onaranays@gmail.com>
2025-09-28 23:25:24 +0300

    Add cookie consent modal.

--- app/layout.js
+++ app/layout.js
@@ -42,6 +42,7 @@ export default function RootLayout({ children }) {
       gtag('config', 'G-YXYJQJ2LRF');
     `}
         </Script>
+        <script src="https://cmp.osano.com/FctbZX4sas/39fc305e-f88c-42f4-aa36-8efe8e29fc26/osano.js"></script>
         <link rel="stylesheet" href="https://use.typekit.net/qkm8iol.css" />
       </head>
       <body className="prose">
